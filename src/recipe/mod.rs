4933584fe5880a6eb6e528c6551aecf3199e069c
Kilian Ischtschuk <kilian.ischtschuk@gmail.com>
2024-10-28 07:41:50 +0100

    feat(recipe): add getter methods

--- src/recipe/mod.rs
+++ src/recipe/mod.rs
@@ -9,6 +9,17 @@ pub struct Tag {
     name: String,
     slug: String,
 }
+impl Tag {
+    pub fn get_id(&self) -> &uuid::Uuid {
+        &self.id
+    }
+    pub fn get_name(&self) -> &str {
+        &self.name
+    }
+    pub fn get_slug(&self) -> &str {
+        &self.slug
+    }
+}
 
 #[derive(Debug, Clone, Serialize, Deserialize, TypedBuilder)]
 #[serde(rename_all = "camelCase")]
@@ -24,6 +35,38 @@ pub struct RecipeIngredient {
     original_text: Option<String>,
     reference_id: Option<uuid::Uuid>,
 }
+impl RecipeIngredient {
+    pub fn get_quantity(&self) -> f64 {
+        self.quantity
+    }
+    pub fn get_unit(&self) -> &Option<String> {
+        &self.unit
+    }
+    pub fn get_food(&self) -> &Option<String> {
+        &self.food
+    }
+    pub fn get_note(&self) -> &str {
+        &self.note
+    }
+    pub fn get_is_food(&self) -> bool {
+        self.is_food
+    }
+    pub fn get_disable_amount(&self) -> bool {
+        self.disable_amount
+    }
+    pub fn get_display(&self) -> &str {
+        &self.display
+    }
+    pub fn get_title(&self) -> &Option<String> {
+        &self.title
+    }
+    pub fn get_original_text(&self) -> &Option<String> {
+        &self.original_text
+    }
+    pub fn get_reference_id(&self) -> &Option<uuid::Uuid> {
+        &self.reference_id
+    }
+}
 
 #[derive(Debug, Clone, Serialize, Deserialize, TypedBuilder, Default)]
 #[serde(rename_all = "camelCase")]
@@ -31,6 +74,17 @@ pub struct RecipeInstruction {
     id: uuid::Uuid,
     title: String,
     text: String,
+}
+impl RecipeInstruction {
+    pub fn get_id(&self) -> &uuid::Uuid {
+        &self.id
+    }
+    pub fn get_title(&self) -> &str {
+        &self.title
+    }
+    pub fn get_text(&self) -> &str {
+        &self.text
+    }
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize, TypedBuilder)]
@@ -55,6 +109,65 @@ pub struct Recipe {
     recipe_ingredient: Vec<RecipeIngredient>,
     recipe_instructions: Vec<RecipeInstruction>,
 }
+impl Recipe {
+    pub fn get_id(&self) -> &uuid::Uuid {
+        &self.id
+    }
+    pub fn get_user_id(&self) -> &uuid::Uuid {
+        &self.user_id
+    }
+    pub fn get_household_id(&self) -> &uuid::Uuid {
+        &self.household_id
+    }
+    pub fn get_group_id(&self) -> &uuid::Uuid {
+        &self.group_id
+    }
+    pub fn get_name(&self) -> &str {
+        &self.name
+    }
+    pub fn get_slug(&self) -> &str {
+        &self.slug
+    }
+    pub fn get_image(&self) -> &str {
+        &self.image
+    }
+    pub fn get_recipe_yield(&self) -> &str {
+        &self.recipe_yield
+    }
+    pub fn get_total_time(&self) -> &str {
+        &self.total_time
+    }
+    pub fn get_prep_time(&self) -> &str {
+        &self.prep_time
+    }
+    pub fn get_cook_time(&self) -> &Option<String> {
+        &self.cook_time
+    }
+    pub fn get_perform_time(&self) -> &str {
+        &self.perform_time
+    }
+    pub fn get_description(&self) -> &str {
+        &self.description
+    }
+    pub fn get_tags(&self) -> &Vec<Tag> {
+        &self.tags
+    }
+    pub fn get_org_url(&self) -> &str {
+        &self.org_url
+    }
+    pub fn get_recipe_ingredient(&self) -> &Vec<RecipeIngredient> {
+        &self.recipe_ingredient
+    }
+    pub fn get_recipe_instructions(&self) -> &Vec<RecipeInstruction> {
+        &self.recipe_instructions
+    }
+    pub fn from_json_file(path: &str) -> RecipeResult<Self> {
+        let file = File::open(path)?;
+        let reader = BufReader::new(file);
+        let recipe: Self = serde_json::from_reader(reader)?;
+        Ok(recipe)
+    }
+}
 
 #[derive(thiserror::Error, Debug)]
 pub enum RecipeError {
@@ -65,12 +178,3 @@ pub enum RecipeError {
 }
 
 pub type RecipeResult<T> = Result<T, RecipeError>;
-
-impl Recipe {
-    pub fn from_json_file(path: &str) -> RecipeResult<Self> {
-        let file = File::open(path)?;
-        let reader = BufReader::new(file);
-        let recipe: Self = serde_json::from_reader(reader)?;
-        Ok(recipe)
-    }
-}
